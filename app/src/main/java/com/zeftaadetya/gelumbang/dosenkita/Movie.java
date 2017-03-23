77d2b31090f8b3d2c05abda772967b4b240a46bf
ALGHI <ALGHI>
2017-03-23 16:31:37 +0700

    Initial commit

---
+++ app/src/main/java/com/zeftaadetya/gelumbang/dosenkita/Movie.java
@@ -0,0 +1,37 @@
+package com.zeftaadetya.gelumbang.dosenkita;
+
+/**
+ * Created by acer on 22/03/2017.
+ */
+
+public class Movie {
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getYear() {
+        return Year;
+    }
+
+    public void setYear(String year) {
+        Year = year;
+    }
+
+    private String title;
+    private String genre;
+    private String Year;
+
+
+}
